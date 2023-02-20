f69f070f7aaf2462188fef2e33a2c205e921ba9f
Selcuk ARSLAN <slckarslan93@gmail.com>
2023-02-20 13:58:28 +0300

    TryGetValue,GetOrCreate,remove

--- InMemoryCache/Controllers/ProductController.cs
+++ InMemoryCache/Controllers/ProductController.cs
@@ -14,6 +14,28 @@ namespace InMemoryCache.Controllers
 
         public IActionResult Index()
         {
+            //1. Yol
+            if (string.IsNullOrEmpty(_memoryCache.Get<string>("zaman")))
+            {
+                _memoryCache.Set<string>("zaman", DateTime.Now.ToString());
+            }
+            //2. Yol
+            if (!_memoryCache.TryGetValue("zaman", out string zamanCache))
+            {
+                _memoryCache.Set<string>("zaman", DateTime.Now.ToString());
+            }
+
+            return View();
+        }
+
+        public IActionResult Show()
+        {
+            _memoryCache.GetOrCreate<string>("zaman", entry =>
+            {
+                return DateTime.Now.ToString();
+            });
+
+            ViewBag.zaman = _memoryCache.Get<string>("zaman");
             return View();
         }
     }
