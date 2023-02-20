19b987305ea78a67a2d2c2639e4e62537329e42b
Selcuk ARSLAN <slckarslan93@gmail.com>
2023-02-20 14:34:38 +0300

    AbsoluteExpiration,SlidingExpiration

--- InMemoryCache/Controllers/ProductController.cs
+++ InMemoryCache/Controllers/ProductController.cs
@@ -14,15 +14,20 @@ namespace InMemoryCache.Controllers
 
         public IActionResult Index()
         {
-            //1. Yol
-            if (string.IsNullOrEmpty(_memoryCache.Get<string>("zaman")))
-            {
-                _memoryCache.Set<string>("zaman", DateTime.Now.ToString());
-            }
+            ////1. Yol
+            //if (string.IsNullOrEmpty(_memoryCache.Get<string>("zaman")))
+            //{
+            //    _memoryCache.Set<string>("zaman", DateTime.Now.ToString());
+            //}
             //2. Yol
             if (!_memoryCache.TryGetValue("zaman", out string zamanCache))
             {
-                _memoryCache.Set<string>("zaman", DateTime.Now.ToString());
+                MemoryCacheEntryOptions options = new MemoryCacheEntryOptions();
+                options.AbsoluteExpiration = DateTime.Now.AddMinutes(1);
+
+                options.SlidingExpiration = TimeSpan.FromSeconds(10);
+
+                _memoryCache.Set<string>("zaman", DateTime.Now.ToString(), options);
             }
 
             return View();
@@ -30,12 +35,16 @@ namespace InMemoryCache.Controllers
 
         public IActionResult Show()
         {
-            _memoryCache.GetOrCreate<string>("zaman", entry =>
-            {
-                return DateTime.Now.ToString();
-            });
+            //_memoryCache.GetOrCreate<string>("zaman", entry =>
+            //{
+            //    return DateTime.Now.ToString();
+            //});
 
-            ViewBag.zaman = _memoryCache.Get<string>("zaman");
+            _memoryCache.TryGetValue("zaman", out string zamanCache);
+
+            ViewBag.zaman = zamanCache;
+
+            //ViewBag.zaman = _memoryCache.Get<string>("zaman");
             return View();
         }
     }
