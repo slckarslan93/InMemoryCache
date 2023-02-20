c168e2bb6a7398ddc8e061be7cddbe8ccabeb0f7
Selcuk ARSLAN <slckarslan93@gmail.com>
2023-02-20 13:34:52 +0300

    IMemoryCache

---
+++ InMemoryCache/Controllers/ProductController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace InMemoryCache.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IMemoryCache _memoryCache;
+
+        public ProductController(IMemoryCache memoryCache)
+        {
+            _memoryCache = memoryCache;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
